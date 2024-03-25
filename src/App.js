db27af3de1fd976311fd046f5692bc220eb4f9f7
Randi Gabbard <gabbardr@msu.edu>
2024-03-25 14:14:16 -0400

    Test Deployment with Changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is my FIRST React App and I learned how to do it from Professor Siarto!
         </a>
       </header>
     </div>
